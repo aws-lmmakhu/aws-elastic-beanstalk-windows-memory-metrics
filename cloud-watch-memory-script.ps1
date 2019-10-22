f257cb18759ba8541c0687137f416323ef4c81c2
lmmakhu <lmmakhu@amazon.com>
2019-10-22 14:57:59 +0200

    added sample files

---
+++ cloud-watch-memory-script.ps1
@@ -0,0 +1,4 @@
+cd "C:\\Program Files\\Amazon\\AmazonCloudWatchAgent"     
+./amazon-cloudwatch-agent-ctl.ps1 -a -config -m ec2 -c file:cw-memory-config.json -s
+./amazon-cloudwatch-agent-ctl.ps1 -a start
+exit
---
+++ copy-cloud-watch-config-script.ps1
@@ -0,0 +1,2 @@
+copy-item -path "C:\staging\.ebextensions\cw-memory-config.json" -destination "C:\Program Files\Amazon\AmazonCloudWatchAgent\cw-memory-config.json"
+exit
