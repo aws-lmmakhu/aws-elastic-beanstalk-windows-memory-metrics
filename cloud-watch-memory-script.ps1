53d687ee36d106c53fcaf39dc8c74edc9b98a97a
lmmakhu <lmmakhu@amazon.com>
2019-10-23 14:25:25 +0200

    added configuration files

--- cloud-watch-memory-script.ps1
+++ cloud-watch-memory-script.ps1
@@ -1,4 +1,4 @@
-cd "C:\\Program Files\\Amazon\\AmazonCloudWatchAgent"     
+Set-Location -Path "C:\\Program Files\\Amazon\\AmazonCloudWatchAgent"     
 ./amazon-cloudwatch-agent-ctl.ps1 -a -config -m ec2 -c file:cw-memory-config.json -s
 ./amazon-cloudwatch-agent-ctl.ps1 -a start
 exit
